14ac1681629f5fceeb186c8682b3fda9705cbddb
Kai Ellinger <kellinge@kellinge-lnxdsk.(none)>
2012-08-29 12:31:50 +0200

    added line 0.. for line numbers from FichteFoll

--- copy_with_line_numbers/copy_with_line_numbers.py
+++ copy_with_line_numbers/copy_with_line_numbers.py
@@ -4,6 +4,7 @@ import os
 class CopyWithLineNumbersCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = sublime.Window.active_view(sublime.active_window())
+        sels = self.view.sel()
 
         # set file name
         if view.file_name():
@@ -11,9 +12,14 @@ class CopyWithLineNumbersCommand(sublime_plugin.TextCommand):
         else:
         	output = "File: <unsaved>\n"
 
+        # To print all the line numbers with the same lenght
+        max_line_num = self.get_line_num(sels[-1].end())
+        max_line_num_len = len(str(max_line_num))
+        format_string = "%0" + str(max_line_num_len) + "d: %s\n"
+
         # handle text
         isFollowupSelection = None
-        for selection in view.sel():
+        for selection in sels:
             if isFollowupSelection:
             	# split multi selections with ---
             	output += "---\n"
@@ -21,8 +27,14 @@ class CopyWithLineNumbersCommand(sublime_plugin.TextCommand):
             	# but not the first one
             	isFollowupSelection = True
             # for each selection
-            for line in view.lines(selection):
-                output += str(view.rowcol(line.begin())[0] + 1) + ": " + view.substr(line) + "\n"
+            selection = self.view.line(selection)  # Extend selection to full lines
+            first_line_num = self.get_line_num(selection.begin())
+            lines = self.view.substr(selection).split("\n")  # Considers all line breaks
+            for i, line in enumerate(lines):
+                output += format_string % (first_line_num + i, line)
 
         # send to clipboard
         sublime.set_clipboard(output)
+
+    def get_line_num(self, point):
+        return self.view.rowcol(point)[0] + 1
